015c76d4e43b68fa22c4bc4c61256c3cbc7ed50d
Wade Simmons <wade@wades.im>
2013-09-22 22:15:46 -0700

    cache common precision values

--- const.go
+++ const.go
@@ -6,13 +6,28 @@ import (
 	"math/big"
 )
 
+const (
+	expCacheLen = 20
+)
+
 var (
 	big10  = big.NewInt(10)
 	big100 = big.NewInt(100)
+
+	exp10cache = func() (cache [expCacheLen]*big.Int) {
+		for i := 0; i < expCacheLen; i++ {
+			m := big.NewInt(int64(i))
+			cache[i] = m.Exp(big10, m, nil)
+		}
+		return
+	}()
 )
 
 // Return 10 ** exp
 func exp10(exp int) *big.Int {
+	if exp < expCacheLen {
+		return exp10cache[exp]
+	}
 	m := big.NewInt(int64(exp))
 	return m.Exp(big10, m, nil)
 }
