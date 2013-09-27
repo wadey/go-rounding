77746a865918733f442541a101f0bf00f43a5bba
Wade Simmons <wade@wades.im>
2013-09-27 11:27:07 -0700

    clean up RoundingMode
    Make it a function so users can implement their own. Makes the code
    clearer as a bonus.

--- round.go
+++ round.go
@@ -4,24 +4,107 @@ import (
 	"math/big"
 )
 
-type RoundingMode int
+// RoundingMode describes how to round a given number.
+// n is the integer that should be rounded such that the last digit is zero.
+// l is the current last digit of n. For most cases you shouldn't need to
+// implement this yourself, use one of the provided implementations in the
+// rounding package.
+type RoundingMode func(n, l *big.Int)
 
-const (
-	// Rounding mode to round away from zero.
-	Up RoundingMode = iota
-	// Rounding mode to round towards zero.
-	Down
-	// Rounding mode to round towards positive infinity.
-	Ceil
-	// Rounding mode to round towards negative infinity.
-	Floor
-	// Rounding mode to round towards "nearest neighbor" unless both neighbors are equidistant, in which case round up.
-	HalfUp
-	// Rounding mode to round towards "nearest neighbor" unless both neighbors are equidistant, in which case round down.
-	HalfDown
-	// Rounding mode to round towards the "nearest neighbor" unless both neighbors are equidistant, in which case, round towards the even neighbor.
-	HalfEven
-)
+func roundUp(n, l *big.Int) {
+	li := l.Int64()
+	if li != 0 {
+		if n.Sign() > 0 {
+			n.Add(n, l.SetInt64(10-li))
+		} else {
+			n.Sub(n, l.SetInt64(10-li))
+		}
+	}
+}
+
+func roundDown(n, l *big.Int) {
+	if n.Sign() > 0 {
+		n.Sub(n, l)
+	} else {
+		n.Add(n, l)
+	}
+}
+
+func roundCeil(n, l *big.Int) {
+	li := l.Int64()
+	if n.Sign() > 0 {
+		if li != 0 {
+			n.Add(n, l.SetInt64(10-li))
+		}
+	} else {
+		n.Add(n, l)
+	}
+}
+
+func roundFloor(n, l *big.Int) {
+	if n.Sign() > 0 {
+		n.Sub(n, l)
+	} else {
+		li := l.Int64()
+		if li != 0 {
+			n.Sub(n, l.SetInt64(10-li))
+		}
+	}
+}
+
+func roundHalfUp(n, l *big.Int) {
+	if l.Int64() >= 5 {
+		roundUp(n, l)
+	} else {
+		roundDown(n, l)
+	}
+}
+
+func roundHalfDown(n, l *big.Int) {
+	if l.Int64() > 5 {
+		roundUp(n, l)
+	} else {
+		roundDown(n, l)
+	}
+}
+
+func roundHalfEven(n, l *big.Int) {
+	li := l.Int64()
+	if li == 5 {
+		k := new(big.Int).Rem(n, big100)
+		ki := k.Int64() / 10
+		if ki%2 == 0 {
+			roundDown(n, l)
+		} else {
+			roundUp(n, l)
+		}
+	} else if li > 5 {
+		roundUp(n, l)
+	} else {
+		roundDown(n, l)
+	}
+}
+
+// Rounding mode to round away from zero.
+var Up RoundingMode = roundUp
+
+// Rounding mode to round towards zero.
+var Down RoundingMode = roundDown
+
+// Rounding mode to round towards positive infinity.
+var Ceil RoundingMode = roundCeil
+
+// Rounding mode to round towards negative infinity.
+var Floor RoundingMode = roundFloor
+
+// Rounding mode to round towards "nearest neighbor" unless both neighbors are equidistant, in which case round up.
+var HalfUp RoundingMode = roundHalfUp
+
+// Rounding mode to round towards "nearest neighbor" unless both neighbors are equidistant, in which case round down.
+var HalfDown RoundingMode = roundHalfDown
+
+// Rounding mode to round towards the "nearest neighbor" unless both neighbors are equidistant, in which case, round towards the even neighbor.
+var HalfEven RoundingMode = roundHalfEven
 
 // Round sets x to its value rounded to the given precision using the given rounding mode.
 // Returns x, which was modified in place.
@@ -30,69 +113,8 @@ func Round(x *big.Rat, prec int, method RoundingMode) *big.Rat {
 	n, d := x.Num(), x.Denom()
 	l := new(big.Int).Rem(n, big10)
 	l.Abs(l)
-	li := l.Int64()
 
-	switch method {
-	case HalfUp:
-		if li >= 5 {
-			method = Up
-		} else {
-			method = Down
-		}
-	case HalfDown:
-		if li > 5 {
-			method = Up
-		} else {
-			method = Down
-		}
-	case HalfEven:
-		if li == 5 {
-			k := new(big.Int).Rem(n, big100)
-			ki := k.Int64() / 10
-			if ki%2 == 0 {
-				method = Down
-			} else {
-				method = Up
-			}
-		} else if li > 5 {
-			method = Up
-		} else {
-			method = Down
-		}
-	}
-
-	switch method {
-	case Up:
-		if li != 0 {
-			if x.Sign() > 0 {
-				n.Add(n, l.SetInt64(10-li))
-			} else {
-				n.Sub(n, l.SetInt64(10-li))
-			}
-		}
-	case Down:
-		if x.Sign() > 0 {
-			n.Sub(n, l)
-		} else {
-			n.Add(n, l)
-		}
-	case Ceil:
-		if x.Sign() > 0 {
-			if li != 0 {
-				n.Add(n, l.SetInt64(10-li))
-			}
-		} else {
-			n.Add(n, l)
-		}
-	case Floor:
-		if x.Sign() > 0 {
-			n.Sub(n, l)
-		} else {
-			if li != 0 {
-				n.Sub(n, l.SetInt64(10-li))
-			}
-		}
-	}
+	method(n, l)
 
 	// To force renormalization
 	return x.SetFrac(n, d)
