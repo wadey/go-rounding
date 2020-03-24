85ae4f325fc1c817165841a33bb4be419cc86b7f
Karel Bilek <karel.bilek@brank.as>
2020-03-25 00:54:44 +0700

    Better test printing (#9)
    * Better test printing
    
    The current test results are confusing as they are written as rationals.
    Let's write them as float strings.
    
    * Update round_test.go
    
    lets print both args the same to make it even easier
    
    Co-authored-by: Wade Simmons <wade@wades.im>

--- round_test.go
+++ round_test.go
@@ -184,7 +184,7 @@ func testRounding(t *testing.T, a, b string, prec int, method RoundingMode) {
 	Round(x, prec, method)
 
 	if x.Cmp(y) != 0 {
-		t.Errorf("test Round(%v, %v, %v) == %s. Got %v", a, prec, method, y, x)
+		t.Errorf("test Round(%v, %v, %v) == %s. Got %v", a, prec, method, y.FloatString(3), x.FloatString(3))
 	}
 }
 
